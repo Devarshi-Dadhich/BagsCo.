891360eb8b4530ae10d0c1f0e2ef34b65e39697f
Devarshi-Dadhich <devarshidadhich2010@gmail.com>
2025-06-23 16:13:51 +0530

    process env

--- app.js
+++ app.js
@@ -41,5 +41,7 @@ app.use('/users',userRouter)
 app.use('/vendors',vendorRouter)
 app.use('/products',productRouter)
 
-const PORT = 3000;
-app.listen(PORT)
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
